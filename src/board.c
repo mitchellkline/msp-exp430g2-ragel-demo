9ce28b33f06d21be9dbc47a6c338a7d6ac800a14
Mitchell Kline <mitchellkline@gmail.com>
2014-08-17 10:07:41 -0700

    MSP430 library from TI incompatible with G series.

---
+++ src/board.c
@@ -0,0 +1,22 @@
+#include "board.h"
+
+void led_init(void)
+{
+  P1DIR |= (P1_LED_RED | P1_LED_GRN);                          
+  P1OUT &= ~(P1_LED_RED | P1_LED_GRN);  
+}
+
+void button_init(void)                 // Configure Push Button 
+{
+  P1DIR &= ~P1_BUTTON;
+  P1OUT |= P1_BUTTON;
+  P1REN |= P1_BUTTON;
+  P1IES |= P1_BUTTON;
+  P1IFG &= ~P1_BUTTON;
+  P1IE |= P1_BUTTON;
+}
+
+void board_init(void) {
+  led_init();
+  button_init();
+}
---
+++ src/board.h
@@ -0,0 +1,17 @@
+#ifndef __BOARD_H__
+#define __BOARD_H__
+
+#include <msp430.h>
+
+#define     P1_LED_RED            BIT0
+#define     P1_LED_GRN            BIT6
+
+#define     P1_BUTTON             BIT3
+
+#define     P1_TXD                BIT1                      // TXD on P1.1
+#define     P1_RXD                BIT2                      // RXD on P1.2
+
+void led_init(void);
+void button_init(void);
+void board_init(void);
+#endif
